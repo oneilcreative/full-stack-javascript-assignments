8a84941842911c6629ef2c7edb4fdafa36c89d3b
oneilcreative <oneilcreative14@gmail.com>
2017-02-08 20:46:12 -0700

    Java.assignment 1

---
+++ src/javascript/intro/grammer/typings/Java.ass1.js
@@ -0,0 +1,22 @@
+export default {
+  isTrue: true,
+  isFalse: false,
+  email: 'oneilgraphix@gmail.com',
+  age: 42,
+  height: 72,
+  phone: '8015298152',
+  favoritecars: ['camaro', 'corvette', 'Porche'],
+  name: {
+    first: 'Mark',
+    last: 'ONeil',
+  },
+  notDefined: undefined,
+  nullable: null,
+  greet() {
+    return 'Hello';
+  },
+  emptyString: '',
+  zero: 0,
+  one: 1
+  
+};
