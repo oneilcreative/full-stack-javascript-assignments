0807cb5b1abdad32b09201cf93e00505187d0d47
oneilcreative <oneilcreative14@gmail.com>
2017-02-08 21:00:15 -0700

    begin.Java ass

---
+++ src/javascript/intro/grammer/variables/begin.Java.js
@@ -0,0 +1,27 @@
+/*  This is where the discription of what the code is in this file
+* In this case we are creating a few variables using var, let, and const.
+* Then we are setting the variables to different values.
+*
+*
+Author: Mark Oneil
+* Date last updated:2-3-17
+*/
+
+
+
+
+
+//This is a var, we shouldn't use var
+export var message = 'hello';
+export let greeting = 'Yo';
+export let saying ='what up';
+message = 'world';
+// console.log (meassage why we did something or used something)
+greeting = 'hello';
+//console.log: (meassage)
+saying = 'Booyah';
+// this is a comment line 
+// console.log (message);
+
+
+// console.log(greeting);
