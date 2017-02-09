033709183e393bb80a1e415354a340e7f3aeb8c5
oneilcreative <oneilcreative14@gmail.com>
2017-02-08 20:29:44 -0700

    java.as1

---
+++ src/javascript/intro/installing/java.as1.js
@@ -0,0 +1,12 @@
+export const name = 'Mark ONeil';
+
+export const email = 'oneilcreative14@gmail.com';
+
+export const phone = '801-529-8152';
+
+// Write code here
+
+
+console.log(name);
+console.log(email);
+console.log(phone);
